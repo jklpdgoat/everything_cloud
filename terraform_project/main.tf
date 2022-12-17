3baed6e9ac7ddbe61a5becb2e8ece4a4923a9f50
jklpdgoat <jarpadul@gmail.com>
2022-12-17 21:41:20 +0800

    Multi VPC's and Subnet deploy

--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -2,11 +2,43 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-02045ebddb047018b"
-  instance_type = "t2.micro"
+# resource "aws_instance" "my-first-server" {
+#   ami           = "ami-02045ebddb047018b"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     # Name = "Ubuntu"
+#   }
+# }
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
 
   tags = {
-    # Name = "Ubuntu"
+    Name = "prod-subnet"
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id     = aws_vpc.second-vpc.id
+  cidr_block = "10.1.1.0/24"
+
+  tags = {
+    Name = "dev-subnet"
+  }
+}
+
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_vpc" "second-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dev"
   }
 }
