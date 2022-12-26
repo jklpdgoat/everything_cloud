15cb3aa48d41f9c070988f860934db44b1cad8bb
jklpdgoat <jarpadul@gmail.com>
2022-12-26 22:54:53 +0800

    Terraform apply

---
+++ learn-terraform-apply/main.tf
@@ -0,0 +1,25 @@
+provider "docker" {}
+
+provider "random" {}
+
+provider "time" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "random_pet" "nginx" {
+  length = 2
+}
+
+resource "docker_container" "nginx" {
+  count = 4
+  image = docker_image.nginx.latest
+  name  = "nginx-${random_pet.nginx.id}-${count.index}"
+
+  ports {
+    internal = 80
+    external = 8000 + count.index
+  }
+}
