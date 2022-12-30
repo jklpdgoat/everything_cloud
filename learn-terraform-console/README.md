a13a1f9776eb2e8fd4851ca9a5c412c608c8f6e5
jklpdgoat <jarpadul@gmail.com>
2022-12-30 19:18:46 +0800

    Start develop configuration with the Console

---
+++ learn-terraform-console/README.md
@@ -0,0 +1,6 @@
+## Learn Terraform - The Console Command
+
+The Terraform console provides an interpreter that you can use to evaluate
+Terraform expressions and explore your Terraform project's state.
+
+Follow along with this Hashicorp [tutorial](https://developer.hashicorp.com/terraform/tutorials/cli/console).
---
+++ learn-terraform-console/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      hashicorp-learn = "console"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "data" {
+  bucket_prefix = var.bucket_prefix
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "data" {
+  bucket = aws_s3_bucket.data.id
+  acl    = "private"
+}
+
+data "aws_s3_objects" "data_bucket" {
+  bucket = aws_s3_bucket.data.bucket
+}
