00046c03fe6c28d05f5e891dcded86507a0daaf2
jklpdgoat <jarpadul@gmail.com>
2023-01-05 13:07:21 +0800

    Open port 8080 for HTTP traffic

--- Packer/demo_proj/terraform_infra/main.tf
+++ Packer/demo_proj/terraform_infra/main.tf
@@ -3,10 +3,22 @@ provider "aws" {
 }
 
 resource "aws_instance" "example-instance" {
-  ami           = "ami-07b79d584cc69e61d"
-  instance_type = "t2.micro"
+  ami                    = "ami-07b79d584cc69e61d"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
     Name = "terraform-infra-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-infra-example-sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
