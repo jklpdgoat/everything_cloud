b480fa6c9c9b7153a88ba6b5ca9d11d96e2d13bb
jklpdgoat <jarpadul@gmail.com>
2023-01-08 08:58:36 +0800

    Canary Deployment done

--- AWS_Services/learn-terraform-advanced-deployments/variables.tf
+++ AWS_Services/learn-terraform-advanced-deployments/variables.tf
@@ -1,3 +1,33 @@
+locals {
+  traffic_dist_map = {
+    blue = {
+      blue  = 100
+      green = 0
+    }
+    blue-90 = {
+      blue  = 90
+      green = 10
+    }
+    split = {
+      blue  = 50
+      green = 50
+    }
+    green-90 = {
+      blue  = 10
+      green = 90
+    }
+    green = {
+      blue  = 0
+      green = 100
+    }
+  }
+}
+
+variable "traffic_distribution" {
+  description = "Levels of traffic distribution"
+  type        = string
+}
+
 variable "region" {
   description = "The region Terraform deploys your instances"
   type        = string
@@ -66,6 +96,18 @@ variable "enable_blue_env" {
 
 variable "blue_instance_count" {
   description = "Number of instances in blue environment"
+  type        = number
+  default     = 2
+}
+
+variable "enable_green_env" {
+  description = "Enable green environment"
+  type        = bool
+  default     = true
+}
+
+variable "green_instance_count" {
+  description = "Number of instances in green environment"
   type        = number
   default     = 2
 }
