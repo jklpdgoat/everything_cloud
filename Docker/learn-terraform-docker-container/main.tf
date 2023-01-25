095d3a36d45f310ea3c4ca075d3876c2b6a9efc6
jklpdgoat <jarpadul@gmail.com>
2023-01-25 23:21:04 +0800

    Add main.tf and deploy docker container

--- Docker/learn-terraform-docker-container/main.tf
+++ Docker/learn-terraform-docker-container/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 2.13.0"
     }
   }
@@ -10,5 +10,15 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-  name = "nginx
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
 }
