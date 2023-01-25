e8694634942b7e646528a294d75a70853e816967
jklpdgoat <jarpadul@gmail.com>
2023-01-25 23:14:45 +0800

    Learn Docker Terraform

---
+++ Docker/learn-terraform-docker-container/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx
+}
